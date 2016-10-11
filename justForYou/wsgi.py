8c26704c50b4e866be532efed55c561a68dd181c
krazzzeee <duane0161@gmail.com>
2016-10-10 21:16:40 -0300

    Update wsgi.py

--- justForYou/wsgi.py
+++ justForYou/wsgi.py
@@ -19,37 +19,6 @@ application = get_wsgi_application()
 
 
 
-# import os
-# import sys
-# import site
-#
-# vepath = '/home/mjl/sports/iathlete_blackhawks_venv/lib/python2.7/site-packages'
-#
-# prev_sys_path = list(sys.path)
-# # add the site-packages of our virtualenv as a site dir
-# site.addsitedir(vepath)
-# # add the app's directory to the PYTHONPATH
-# #sys.path.append('/path/to/app/%s/' % ('stage' if STAGE else 'live')
-# sys.path.append('/home/mjl/sports/iathlete_blackhawks_venv/iathlete_blackhawks/')
-# sys.path.append('/home/mjl/sports/iathlete_blackhawks_venv/iathlete_blackhawks/iathlete_blackhawks/')
-#
-# # reorder sys.path so new directories from the addsitedir show up first
-# new_sys_path = [p for p in sys.path if p not in prev_sys_path]
-# for item in new_sys_path:
-#     sys.path.remove(item)
-# sys.path[:0] = new_sys_path
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iathlete_blackhawks.settings")
-#
-# # This application object is used by any WSGI server configured to use this
-# # file. This includes Django's development server, if the WSGI_APPLICATION
-# # setting points here.
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
-
-
-
-
 
 
 
